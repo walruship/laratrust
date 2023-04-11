9bcc91fa5b3b23eaebea954becef0418f099c265
Son T. Tran <286.trants@gmail.com>
2023-04-11 15:33:48 +0700

    Configs

--- src/Laravel/Facades/Laratrust.php
+++ src/Laravel/Facades/Laratrust.php
@@ -1,21 +1,16 @@
 <?php
-
 /*
- * Part of the Laratrust package.
+ * Walruship Co., Ltd
  *
  * NOTICE OF LICENSE
  *
- * Licensed under the 3-clause BSD License.
+ * This source file is subject to the 3-clause BSD license that is
+ * available through the world-wide-web at this URL:
+ * https://walruship.com/LICENSE.txt
  *
- * This source file is subject to the 3-clause BSD License that is
- * bundled with this package in the LICENSE file.
- *
- * @package    Laratrust
- * @version    7.0.0
- * @author     Walruship LLC
- * @license    BSD License (3-clause)
- * @copyright  (c) 2011-2023, Walruship LLC
- * @link       https://cartalyst.com
+ * @author          Walruship Co., Ltd
+ * @copyright       Copyright (c) 2023 Walruship
+ * @link            https://walruship.com
  */
 
 namespace Walruship\Laratrust\Laravel\Facades;
